069341039924c14af40c0948035e5b233c6ccd1c
Miguel PL <mig.parlop@gmail.com>
2022-09-11 13:29:31 -0500

    operadores en python

---
+++ laboratorio-2/operadores.py
@@ -0,0 +1,9 @@
+x = 10; y = 3
+print("Operadores aritméticos")
+print("x+y =", x+y)   #13
+print("x-y =", x-y)   #7
+print("x*y =", x*y)   #30
+print("x/y =", x/y)   #3.3333333333333335
+print("x%y =", x%y)   #1
+print("x**y =", x**y) #1000
+print("x//y =", x//y) #3
